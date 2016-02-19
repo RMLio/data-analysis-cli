cbbca66a7722fa37222ef01c4282281a4a0d3b8c
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-02-19 11:47:08 +0100

    making use of multi-level branch of key-discovery

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var path = require('path');
 var pkg = require(path.join(__dirname, 'package.json'));
+var memwatch = require('memwatch-next');
 var program = require('commander');
 var fs = require('fs');
 const util = require('util');
@@ -14,7 +15,9 @@ var file;
 
 program.version(pkg.version);
 program.usage("<file ...>");
-program.option("-n, --node <method>", "The XPath of the node to be processed.");
+program.option("-n, --node <XPath>", "The XPath of the node to be processed.");
+program.option("-k, --nokeys", "Don't print keys.");
+program.option("-s, --stats", "Print statics.");
 program.action(function (userFile) {
   file = userFile;
 });
@@ -27,41 +30,63 @@ if (!program.node) {
   console.log("No file provided.");
 } else {
   var data = fs.readFileSync(file, 'utf8');
-  data = data.replace(/(\r\n|\n|\r)/gm,"");
+  data = data.replace(/(\r\n|\n|\r)/gm, "");
   data = data.replace(/> *</g, "><");
 
+  if (global.gc) {
+    global.gc();
+  } else {
+    console.log('Garbage collection unavailable.  Pass --expose-gc '
+      + 'when launching node to enable forced garbage collection.');
+  }
+
   console.log("Using " + file + "\n");
+  var finished = false;
+
+  memwatch.on('stats', function (stats) {
+    if (!finished && program.stats) {
+      console.log(stats);
+    }
+  });
 
   var startMemUsage = process.memoryUsage();
   var startTime = new Date().getTime();
 
   var discovery = new XMLKeyDiscovery(data);
-  var results = discovery.discover(program.node);
+  var output = discovery.discover(program.node, {extendedOutput: true});
+  finished = true;
+  var results = output.keys;
 
   var stopTime = new Date().getTime();
   var stopMemUsage = process.memoryUsage();
 
-  console.log("===========");
-  console.log("statistics:");
+  if (program.stats) {
+    console.log("===========");
+    console.log("statistics:");
 
-  console.log("\ttime: " + (stopTime - startTime) + "ms");
+    console.log("\t# nodes: " + output.nodeCount);
+    console.log("\ttime: " + (stopTime - startTime) + "ms");
 
-  stopMemUsage.heapUsed -= startMemUsage.heapUsed;
-  stopMemUsage.heapTotal -= startMemUsage.heapTotal;
-  stopMemUsage.rss -= startMemUsage.rss;
-  console.log("\theapUsed: " + stopMemUsage.heapUsed + " bytes");
-  console.log("\theapTotal: " + stopMemUsage.heapTotal + " bytes");
-  console.log("\trss: " + stopMemUsage.rss + " bytes");
-  console.log("===========");
-  console.log("keys found: " + results.length);
+    stopMemUsage.heapUsed -= startMemUsage.heapUsed;
+    stopMemUsage.heapTotal -= startMemUsage.heapTotal;
+    stopMemUsage.rss -= startMemUsage.rss;
+    console.log("\theapUsed: " + stopMemUsage.heapUsed + " bytes");
+    console.log("\theapTotal: " + stopMemUsage.heapTotal + " bytes");
+    console.log("\trss: " + stopMemUsage.rss + " bytes");
+  }
 
-  for (var i = 0; i < results.length; i ++) {
-    var r = "\t- " + results[i][0];
+  if (!program.nokeys) {
+    console.log("===========");
+    console.log("keys found: " + results.length);
 
-    for (var j = 1; j < results[i].length; j ++) {
-      r += results[i][j];
+    for (var i = 0; i < results.length; i++) {
+      var r = "\t- " + results[i][0];
+
+      for (var j = 1; j < results[i].length; j++) {
+        r += results[i][j];
+      }
+
+      console.log(r);
     }
-
-    console.log(r);
   }
 }
